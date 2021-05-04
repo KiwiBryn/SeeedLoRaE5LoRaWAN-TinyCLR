2318295e986e8b4a2b6e0ee153856b6375878cec
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2021-05-05 08:10:25 +1200

    Stubbed out SeeedE5 libary and test harness

---
+++ SeeedE5LoRaWANDeviceClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace SeeedE5LoRaWANDeviceClient
+{
+   class Program
+   {
+      static void Main()
+      {
+      }
+   }
+}
